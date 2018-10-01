f226f4dd77408204d66d5a053396353f8172d8ff
Aleksey Ilyin <alksily@icloud.com>
2018-10-01 14:05:52 +0300

    fix comment

--- src/AbstractFilter.php
+++ src/AbstractFilter.php
@@ -4,6 +4,7 @@ namespace AEngine\Orchid\Filter;
 
 /**
  * @deprecated This class is deprecated, now you need use non abstract class Filter
+ * @see Filter
  */
 abstract class AbstractFilter extends Filter
 {
